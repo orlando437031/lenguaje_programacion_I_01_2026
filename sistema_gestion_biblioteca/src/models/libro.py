dc4b8889dea3f4d320e7b4d7e5f5e551fdc0d71c
Jaime Vilorio Green <89463852+jaimeviloriogreen@users.noreply.github.com>
2026-02-06 13:36:37 -0400

    Delete sistema_gestion_biblioteca directory

--- sistema_gestion_biblioteca/src/models/libro.py
+++
@@ -1,8 +0,0 @@
-"""Anotaciones de tipo del libro en la lista de libros"""
-from typing import TypedDict
-
-class Libro(TypedDict):
-    titulo: str
-    autor: str
-    paginas: int
-    disponible: bool
